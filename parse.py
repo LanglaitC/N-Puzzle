671702b179455c7bfaeac3f004210568c5a0b801
Corentin LANGLAIT <corentin.langlait@gmail.com>
2018-10-27 18:24:00 +0200

    Parsing should be done, test is in progress

--- parse.py
+++ parse.py
@@ -1,17 +1,27 @@
 import sys
 
 def parse_file(file):
-    '''Fonction that parses the file to create a new taquin instance'''
+    '''Fonction that parses the file to check it and return a new taquin instance based on its content'''
     try:
         fd = open(file);
         content = fd.read().split('\n');
     except Exception as e:
-        raise e;
+        raise Exception("File doesn't exist or isn't valid format");
     if content == "":
         raise Exception("File is empty");
-    result = []
+    result = [];
     for line in content:
-        if (line[0] == '#'):
-            print("comment");
+        if (line and line[0] == '#'):       ##  Si la ligne commence par une diese c'est un commentaire
+            pass
         else:
-            result.push(line);
+            convert = [];                   ##  Sinon on verifie que chaque element de la ligne est un nombre entier et on le convertit en int
+            for each in line.split():
+                try:
+                    convert.append(int(each));
+                except Exception as e:
+                    raise e;
+            result.append(convert);
+    if len(result) < 2:
+        raise Exception("File is not valid format");
+    return result;
+                
---
+++ unittest/parse_file.test.py
@@ -0,0 +1,33 @@
+import unittest;
+from parse import *;
+
+test = [[1, 3, 2], [0, 4, 6], [8, 7, 5]];
+
+class TestParseFile(unittest.TestCase):
+    
+    def test_valid_3x3(self):
+        self.assertEqual([[1, 3, 2], [0, 4, 6], [8, 7, 5]], parse_file("testFiles/valids/3x3.txt"));
+    
+    def test_valid_3x3_with_comments(self):
+        self.assertEqual([[1, 3, 2], [0, 4, 6], [8, 7, 5]], parse_file("testFiles/valids/3x3_comment.txt"));
+    
+    def test_empty_file(self):
+        with self.assertRaises(Exception) as context:
+            parse_file("testFiles/invalids/empty_file.txt");
+        self.assertTrue("File is not valid format" in context.exception);
+
+    def test_only_comment(self):
+        with self.assertRaises(Exception) as context:
+            parse_file("testFiles/invalids/only_comment.txt")
+        self.assertTrue("File is not valid format" in context.exception);
+
+    def test_one_dimension_taquin(self):
+        with self.assertRaises(Exception) as context:
+            self.assertRaises(parse_file("testFiles/invalids/1x1_taquin.txt"));
+        self.assertTrue("File is not valid format" in context.exception);
+
+    def test_splited_3x3(self):
+        self.assertRaises(parse_file("testFiles/invalids/splited_3x3.txt"));
+
+if __name__ == '__main__':
+    unittest.main();
