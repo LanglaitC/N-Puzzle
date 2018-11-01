669236498cea40af8691e62e356d72270c751f2b
Corentin LANGLAIT <corentin.langlait@gmail.com>
2018-11-01 13:46:58 +0100

    new tests and improved parsing

--- npuzzle.py
+++ npuzzle.py
@@ -1,4 +1,8 @@
 import parse;
 import sys;
 
-parse.parse_file(sys.argv[1]);
+try:
+    parse.parse_file(sys.argv[1]);   
+except Exception as e:
+    raise e;
+    exit(1);
--- parse.py
+++ parse.py
@@ -1,7 +1,48 @@
 import sys
 
+def check_validity(tab, dim):
+    vert_dim = len(tab);
+    hash_tab = {};
+    if vert_dim < 2:
+        raise Exception("File is not valid format");
+    if vert_dim != dim:
+        raise Exception("Invalid file, taquin should be a square");
+    for i in range(dim):
+        for j in range(dim):
+            if tab[i][j] >= dim * dim or tab[i][j] < 0:
+                raise Exception("Character " + str(tab[i][j])+ " is out of range should be between 0 and " + str(dim * dim - 1) + ", position : (" + str(i + 1) + "," + str(j + 1) + ")");
+            if tab[i][j] not in hash_tab:
+                hash_tab[tab[i][j]] = 1;
+            else:
+                raise Exception("Duplicate Character " + str(tab[i][j]) + " at position : (" + str(i + 1) + "," + str(j + 1) + ") is invalid");
+    return tab;
+
+def parse_line(line):
+    convert = [];
+    for each in line.split():
+        try:
+            convert.append(int(each));
+        except Exception as e:
+            raise e;
+    if len(convert) == 0:
+        raise Exception("Lign is empty");
+    return convert;
+
+def is_comment(line):
+    '''Function that states if a given line is a commnent'''
+    if (not line or not isinstance(line, str)):
+        return False;
+    for each in line:
+        if each in ' \t':
+            pass;
+        elif each == '#':
+            return True;
+        else:
+            return False;
+    return False;
+    
 def parse_file(file):
-    '''Fonction that parses the file to check it and return a new taquin instance based on its content'''
+    '''Function that parses the file to check it and return a new taquin instance based on its content'''
     try:
         fd = open(file);
         content = fd.read().split('\n');
@@ -10,20 +51,15 @@ def parse_file(file):
     if content == "":
         raise Exception("File is empty");
     result = [];
+    dim = None;
     for line in content:
-        if (line and line[0] == '#'):       ##  Si la ligne commence par une diese c'est un commentaire
+        if (is_comment(line)):       ##  Si la ligne commence par une diese c'est un commentaire
             pass
-        else:
-            convert = [];                   ##  Sinon on verifie que chaque element de la ligne est un nombre entier et on le convertit en int
-            for each in line.split():
-                try:
-                    convert.append(int(each));
-                except Exception as e:
-                    raise e;
-            if len(convert) == 0:
-                raise Exception("Lign is empty");
+        else:                        ##  Sinon on verifie que chaque element de la ligne est un nombre entier et on le convertit en int
+            convert = parse_line(line);
+            if dim is not None and dim != len(convert):
+                raise Exception("Invalid file, taquin should be a square");
+            dim = len(convert);
             result.append(convert);
-    if len(result) < 2:
-        raise Exception("File is not valid format");
+    result = check_validity(result, dim);
     return result;
-                
