b0e611a8304454dff62f7e275510f3e15a116fcd
Celia CORSIN <ccorsin@student.42.fr>
2019-01-05 15:56:12 +0100

    generate random if nothing done

--- npuzzle.py
+++ npuzzle.py
@@ -2,10 +2,11 @@ import parse
 import sys
 from model import *
 from puzzle import *
+from generator import createPuzzle
 
 try:
     ##TODO if no argument is given make a random puzzle
-    res = parse.parse_file(sys.argv[1])
+    res = parse.parse_file(sys.argv[1]) if len(sys.argv) > 1 else createPuzzle(3)
     model = Model(len(res)).model
     # print(tak)
 except Exception as e:
